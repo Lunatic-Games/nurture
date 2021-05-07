aa064d831b3d3d1995b4d0926688466868f17777
JoeZlonicky <joe.zlonicky@gmail.com>
2021-05-07 12:01:39 -0700

    Renamed folders and moved assets

---
+++ clickable_items/filler/rocks/rock.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+
+onready var rock_particle = preload("res://clickable_items/filler/rocks/rock_particle.tscn")
+
+
+
+func _on_Area2D_input_event(viewport, event, shape_idx):
+	if (event.is_pressed() and event.button_index == BUTTON_LEFT):
+		
+		# Create the particle
+		var particle = rock_particle.instance()
+		add_child(particle)
+		
+		# Shake the rock
+		$Shake.play("shake")
---
+++ menus/main_menu/main_menu.gd
@@ -0,0 +1,21 @@
+extends Control
+
+onready var options = preload("res://menus/options/options.tscn")
+onready var level_1 = preload("res://levels/level_1.tscn")
+onready var tutorial = preload("res://levels/tutorial.tscn")
+
+
+func _on_Play_pressed():
+	var _scene = get_tree().change_scene_to(level_1)
+
+
+func _on_Options_pressed():
+	var _scene = get_tree().change_scene_to(options)
+
+
+func _on_Quit_pressed():
+	get_tree().quit()
+
+
+func _on_Tutorial_pressed():
+	var _scene = get_tree().change_scene_to(tutorial)
