27ec28e852aa0cec5ada7521b7142bda85cb66dc
Joe <joe.zlonicky@gmail.com>
2021-05-08 15:26:15 -0700

    Improved wind and added minor bloom

--- special_overlays/wind.gd
+++ special_overlays/wind.gd
@@ -1,19 +1,33 @@
 extends Line2D
 
-export (float) var speed = 200.0
+export (float, 200, 2000) var speed = 500.0
+export (float, 0, 100) var amplitude = 25
+export (float, 0, 0.05) var period_multipler = 0.007
+ 
+export (int) var length = 200
+export (int) var n_points = 10
 
-export (int) var n_points
-export (float) var segment_length
+var front_position = 0.0
+var bound = INF  # Can be used to wrap around
 
-onready var path_length = $Path.curve.get_baked_length()
-onready var path_position = path_length
+onready var segment_length = length / n_points
+
 
 func _ready():
+	clear_points()
 	for _i in n_points:
 		add_point(Vector2())
 
+
 func _physics_process(delta):
-	path_position -= speed * delta
-	for i in n_points:
-		set_point_position(i, $Path.curve.interpolate_baked(path_position + i * segment_length))
+	front_position += delta * speed
+	front_position = fmod(front_position, bound)
 	
+	for i in n_points:
+		var x = front_position + i * segment_length
+		var y = amplitude * sin(x * period_multipler)
+		set_point_position(i, Vector2(x, y))
+
+
+func jump_forward(amount):
+	front_position = amount
